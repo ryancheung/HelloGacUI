88ea4944db649d8c7c8e6c9b78d20e1ebc04fa67
ryancheung <ryancheung.go@gmail.com>
2023-01-19 20:45:41 +0800

    CMake build

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#define GAC_HEADER_USE_NAMESPACE
+#include "UI/Source/HelloGacUI.h"
+
+using namespace vl::collections;
+using namespace vl::stream;
+
+void GuiMain()
+{
+    {
+        FileStream fileStream(L"HelloWorld.bin", FileStream::ReadOnly);
+        GetResourceManager()->LoadResourceOrPending(fileStream);
+    }
+    hellogacui::MainWindow window;
+    window.MoveToScreenCenter();
+    GetApplication()->Run(&window);
+}
